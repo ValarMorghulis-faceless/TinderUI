87ad5852332e131547329acd2c05811a217aa995
Giorgi samkhar <padregiorgi777@gmail.com>
2022-12-06 15:16:40 +0400

    Added Full Screen Card View

---
+++ TSUDate/Home/CardView/CardView.swift
@@ -0,0 +1,75 @@
+//
+//  CardView.swift
+//  TSUDate
+//
+//  Created by Giorgi Samkharadze on 05.12.22.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    @State var person: Person
+    
+    @Binding var fullscreenMode: Bool
+    
+    let screenCutoff = (UIScreen.main.bounds.width / 2) * 0.8
+    
+    var body: some View {
+        GeometryReader { geo in
+            
+            if fullscreenMode {
+                Text("FULL SCREEN")
+            }else {
+                CardImageScroller(person: person, fullscreenMode: $fullscreenMode)
+                    .animation(.easeOut(duration: 0.2))
+                    .frame(width: geo.size.width - 20, height: geo.size.height)
+                    .padding(.leading, 10 )
+                    .offset(x: person.x, y: person.y)
+                    .rotationEffect(.degrees(person.degree))
+                    .gesture(
+                        DragGesture()
+                            .onChanged({ value in
+                                withAnimation(.default) {
+                                    person.x = value.translation.width
+                                    person.y = value.translation.height
+                                    withAnimation {
+                                        person.degree = Double((value.translation.width / 25) * -1)
+                                    }
+                                }
+                            })
+                            .onEnded({ value in
+                                withAnimation(.interpolatingSpring(mass: 1.0, stiffness: 50, damping: 10, initialVelocity: 0)) {
+                                    let width = value.translation.width
+                                    
+                                    if width >= 0 && width <= screenCutoff {
+                                        person.x = 0
+                                        person.y = 0
+                                        person.degree = 0
+                                    } else if width <= -1 && width >= -screenCutoff {
+                                        person.x = 0
+                                        person.y = 0
+                                        person.degree = 0
+                                    } else if width > screenCutoff {
+                                        person.x = 500
+                                        person.degree = 12
+                                    } else if width < -screenCutoff {
+                                        person.x = -500
+                                        person.degree = -12
+                                    }
+                                }
+                            })
+                    
+                    
+                    )
+            }
+        }
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView(person: Person.example, fullscreenMode: .constant(false))
+        CardView(person: Person.example, fullscreenMode: .constant(true))
+
+    }
+}
--- TSUDate/Models/Person.swift
+++ TSUDate/Models/Person.swift
@@ -6,20 +6,27 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct Person: Hashable, Identifiable {
     let id = UUID().uuidString
     var name: String
     var imageURLS: [URL]
     var bio : String
+    var distance: Int
+    var bioLong: String
     var age: Int
+    
+    var x: CGFloat = 0.0
+    var y: CGFloat = 0.0
+    var degree: Double = 0.0
 }
 
 extension Person {
-    static let example = Person(name: "LizaKvitka", imageURLS: [URL(string: "https://picsum.photos/400/300")!,URL(string: "https://picsum.photos/400/301")!,URL(string: "https://picsum.photos/400/302")!,URL(string: "https://picsum.photos/400/303")!,URL(string: "https://picsum.photos/400/304")!], bio: "I do not do rugs, I am the drugs", age: 21)
-    static let example2 = Person(name: "Side Chick", imageURLS: [URL(string: "https://picsum.photos/400/301")!], bio: "I like kanye west", age: 25)
+    static let example = Person(name: "LizaKvitka", imageURLS: [URL(string: "https://picsum.photos/400/300")!,URL(string: "https://picsum.photos/400/301")!,URL(string: "https://picsum.photos/400/302")!,URL(string: "https://picsum.photos/400/303")!,URL(string: "https://picsum.photos/400/304")!], bio: "I do not do rugs, I am the drugs", distance: 20, bioLong: "I like kanye west", age: 21)
+    static let example2 = Person(name: "Side Chick", imageURLS: [URL(string: "https://picsum.photos/400/301")!], bio: "I like kanye west", distance: 30, bioLong: "I like kanye west", age: 25)
     
     static let examples: [Person] = [
-        Person.example, Person.example2, Person(name: "Rick", imageURLS: [URL(string: "https://picsum.photos/400/302")!], bio: "I love milfs", age: 17),Person(name: "Sophia", imageURLS: [URL(string: "https://picsum.photos/400/303")!], bio: "I am really boring", age: 25),Person(name: "Mia", imageURLS: [URL(string: "https://picsum.photos/400/304")!], bio: "I Have done bad things", age: 27)]
+        Person.example, Person.example2, Person(name: "Rick", imageURLS: [URL(string: "https://picsum.photos/400/302")!], bio: "I love milfs", distance: 40, bioLong: "I like kanye west", age: 17),Person(name: "Sophia", imageURLS: [URL(string: "https://picsum.photos/400/303")!], bio: "I am really boring", distance: 50, bioLong: "I like kanye west", age: 25),Person(name: "Mia", imageURLS: [URL(string: "https://picsum.photos/400/304")!], bio: "I Have done bad things", distance: 60, bioLong: "I like kanye west", age: 27)]
     
 }
